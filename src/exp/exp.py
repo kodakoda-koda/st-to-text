15d9b2bb74f9d9e3b7704c06f5eed9681c11941c
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-08-13 15:12:27 +0900

    feat: add training and evaluation functions for the model.

---
+++ src/exp/exp.py
@@ -0,0 +1,46 @@
+import torch
+
+
+def train(model, train_loader, optimizer, device):
+
+    model.train()
+    total_loss = 0
+    total_samples = 0
+    for batch in train_loader:
+        st_maps = batch["st_maps"].to(device)
+        input_ids = batch["input_ids"][:, :-1].to(device)
+        attention_mask = batch["attention_mask"][:, :-1].to(device)
+        labels = batch["input_ids"][:, 1:].to(device)
+
+        outputs = model(st_maps=st_maps, input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        total_loss += loss.item()
+        total_samples += input_ids.size(0)
+
+    return total_loss / total_samples
+
+
+def eval(model, eval_loader, device):
+
+    model.eval()
+    total_loss = 0
+    total_samples = 0
+    with torch.no_grad():
+        for batch in eval_loader:
+            st_maps = batch["st_maps"].to(device)
+            input_ids = batch["input_ids"][:, :-1].to(device)
+            attention_mask = batch["attention_mask"][:, :-1].to(device)
+            labels = batch["input_ids"][:, 1:].to(device)
+
+            outputs = model(st_maps=st_maps, input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+            loss = outputs.loss
+
+            total_loss += loss.item()
+            total_samples += input_ids.size(0)
+
+    return total_loss / total_samples
