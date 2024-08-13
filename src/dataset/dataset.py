aadf8b07c85714421609ab3d1bd1c74f0cf9b540
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-08-13 15:05:54 +0900

    feat: Add train/validation split to dataset.py.

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -2,11 +2,11 @@ from torch.utils.data import Dataset
 
 
 class CustomDataset(Dataset):
-    def __init__(self, st_maps, labels, tokenizer):
+    def __init__(self, st_maps, labels, tokenizer, max_length, train_flag=True):
         self.st_maps = st_maps
         self.labels = labels
         self.tokenizer = tokenizer
-        self.__tokenize__()
+        self.__split_tokenize__()
 
     def __len__(self):
         return len(self.data)
@@ -18,11 +18,18 @@ class CustomDataset(Dataset):
             "attention_mask": self.attention_mask[idx],
         }
 
-    def __tokenize__(self):
+    def __split_tokenize__(self):
+        if self.train_flag:
+            self.st_maps = self.st_maps[: int(0.8 * len(self.st_maps))]
+            self.labels = self.labels[: int(0.8 * len(self.labels))]
+        else:
+            self.st_maps = self.st_maps[int(0.8 * len(self.st_maps)) :]
+            self.labels = self.labels[int(0.8 * len(self.labels)) :]
+
         labels = ["<pad>" + label for label in self.labels]
         tokenized_labels = self.tokenizer.batch_encode_plus(
             labels,
-            max_length=128,
+            max_length=self.max_length,
             padding=True,
             truncation=True,
             return_tensors="pt",
