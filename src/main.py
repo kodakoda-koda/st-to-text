2ad06ebde4b0d34e7356f6c92b8278c3e067c2ac
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-08-15 13:51:48 +0900

    refactor: Move create_data to dataset folder

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from transformers import AutoTokenizer, get_cosine_schedule_with_warmup, set_seed
 
-from src.create_data import create_data
+from src.dataset.create_data import create_data
 from src.dataset.dataset import CustomDataset
 from src.exp.exp import eval, train
 from src.model.model import Model
