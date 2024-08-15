d121d7207f95d5d853038f853a9bfcbd781189f8
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-08-15 13:14:06 +0900

    refactor: Rename demands to st_maps in encoder.py

--- src/model/encoder.py
+++ src/model/encoder.py
@@ -22,15 +22,15 @@ class GTformer(nn.Module):
             [GTformer_block(d_model, n_heads, d_ff, dropout, n_locations, 7) for _ in range(n_layers)]
         )
 
-    def forward(self, demands: Tensor) -> BaseModelOutput:
+    def forward(self, st_maps: Tensor) -> BaseModelOutput:
         """
         B, T, L, M, N: batch size, time steps, length of text, number of locations, number of time features
-        demands: (B, T, M)
+        st_maps: (B, T, M)
         """
         for layer in self.layers:
-            demands = layer(demands)
+            st_maps = layer(st_maps)
 
-        outputs = demands[:, -24:]  # (B, 24, M)
+        outputs = st_maps[:, -24:]  # (B, 24, M)
 
         return outputs
 
@@ -47,24 +47,24 @@ class GTformer_block(nn.Module):
         self.t_out = nn.Linear(d_model, n_locations)
         self.s_out = nn.Linear(d_model, 30)
 
-    def forward(self, demands: Tensor) -> Tensor:
+    def forward(self, st_maps: Tensor) -> Tensor:
         """
         B, T, L, M, N: batch size, time steps, length of text, number of locations, number of time features
         demands: (B, T, M)
         """
-        B, T, _ = demands.size()
-        t_demands = self.t_emb(demands)
-        s_demands = self.s_emb(demands.permute(0, 2, 1))
+        B, T, _ = st_maps.size()
+        t_maps = self.t_emb(st_maps)
+        s_maps = self.s_emb(st_maps.permute(0, 2, 1))
 
         t_mask = torch.ones(B, 1, T, T)
         t_mask = torch.triu(t_mask, diagonal=1).bool()
-        t_mask = t_mask.to(t_demands.device)
+        t_mask = t_mask.to(t_maps.device)
 
-        t_demands = self.t_transformer(t_demands, t_mask)
-        s_demands = self.s_transformer(s_demands)
+        t_maps = self.t_transformer(t_maps, t_mask)
+        s_maps = self.s_transformer(s_maps)
 
-        t_out = self.t_out(t_demands)
-        s_out = self.s_out(s_demands)
+        t_out = self.t_out(t_maps)
+        s_out = self.s_out(s_maps)
         out = t_out + s_out.permute(0, 2, 1)
 
         return out
