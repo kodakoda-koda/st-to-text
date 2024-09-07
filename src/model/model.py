5dacb860dad0bcd2e91a44200f2d8abd397c6aaa
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-09-07 17:24:17 +0900

    refactor: Update model to use correct dimension for fn_emb.

--- src/model/model.py
+++ src/model/model.py
@@ -23,10 +23,10 @@ class Model(nn.Module):
         self.gtformer = GTformer(n_layers, d_model, n_heads, d_ff, dropout, n_locations)
         t5_config = T5Config()
         t5_config.decoder_start_token_id = 0
-        t5_config.num_layers = 2
+        t5_config.num_layers = 3
         self.vocab_size = t5_config.vocab_size
         self.t5 = T5ForConditionalGeneration(t5_config)
-        self.fn_emb = nn.Linear(32, self.t5.config.d_model)
+        self.fn_emb = nn.Linear(n_locations, self.t5.config.d_model)
 
     def forward(
         self,
@@ -37,7 +37,7 @@ class Model(nn.Module):
     ) -> Seq2SeqLMOutput:
 
         encoder_outputs = self.gtformer(st_maps)
-        encoder_outputs.last_hidden_state = self.fn_emb(encoder_outputs.last_hidden_state.permute(0, 2, 1))
+        encoder_outputs.last_hidden_state = self.fn_emb(encoder_outputs.last_hidden_state)
 
         outputs = self.t5(
             encoder_outputs=encoder_outputs,
@@ -55,7 +55,7 @@ class Model(nn.Module):
     ) -> Any:
 
         encoder_outputs = self.gtformer(st_maps)
-        encoder_outputs.last_hidden_state = self.fn_emb(encoder_outputs.last_hidden_state.permute(0, 2, 1))
+        encoder_outputs.last_hidden_state = self.fn_emb(encoder_outputs.last_hidden_state)
 
         outputs = self.t5.generate(
             encoder_outputs=encoder_outputs,
