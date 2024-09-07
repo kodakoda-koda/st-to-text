772ada8e8ca3f393d11421ed56d895c0edb5cb79
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-09-07 18:33:26 +0900

    refactor: Update the embedding layer for fn_emb.

--- src/exp/exp_base.py
+++ src/exp/exp_base.py
@@ -49,8 +49,6 @@ class Exp_base:
     def _get_weighted_loss_func(self):
         loss_weight = torch.ones(self.model.vocab_size)
         loss_weight[[6313, 6734, 10823, 993, 2667]] = 5.0
-        # loss_weight[[4347, 4482, 6355, 8525, 11116, 11071, 32100, 11864]] = 5.0
-        # loss_weight[[209, 204, 220, 314, 305, 431, 489, 505]] = 5.0
         loss_weight = loss_weight.to(self.device).to(self.dtype)
 
         loss_func = CustomLoss(loss_weight, self.writer)
