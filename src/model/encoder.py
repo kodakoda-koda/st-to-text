0b4975a4708b5a11d4f19389ed61618ea4fa7c71
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-09-10 00:10:34 +0900

    refactor: Remove unused coordinates in GTformer encoder.

--- src/model/encoder.py
+++ src/model/encoder.py
@@ -22,18 +22,17 @@ class GTformer(nn.Module):
         self.layers = nn.ModuleList(
             [GTformer_block(d_model, n_heads, d_ff, dropout, n_locations) for _ in range(n_layers)]
         )
-        self.fn = nn.Linear(32, lm_d_model)
+        self.fn = nn.Linear(n_locations, lm_d_model)
         self.layer_norm = nn.LayerNorm(lm_d_model)
 
     def forward(self, st_maps: FloatTensor) -> BaseModelOutput:
         """
         B, T, L, M, N: batch size, time steps, length of text, number of locations, number of time features
         st_maps: (B, T, M)
-        coords: (B, M, 2)
         """
         for layer in self.layers:
             st_maps = layer(st_maps)
-        out = self.layer_norm(self.fn(st_maps.permute(0, 2, 1)))
+        out = self.layer_norm(self.fn(st_maps))
 
         return BaseModelOutput(last_hidden_state=out, hidden_states=None, attentions=None)
 
