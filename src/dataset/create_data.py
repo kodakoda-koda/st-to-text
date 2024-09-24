f08f2d81c6fd3c650489810ca570c21f8e7064da
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-09-24 16:21:10 +0900

    fix: Change minimum fluctuation range to 3.

--- src/dataset/create_data.py
+++ src/dataset/create_data.py
@@ -26,7 +26,7 @@ def create_data(time_range: int, max_fluc_range: int, n_data: int, map_size: int
 
     for i in range(n_data):
         # Randomly generate a spot and change
-        fluc_range = np.random.randint(1, max_fluc_range)
+        fluc_range = np.random.randint(3, max_fluc_range)
         fluc_index = np.random.randint(3, time_range - fluc_range - 3)
         start_value = np.random.uniform(0.2, 0.8)
         fluc_list = ["increase", "decrease", "peak", "dip", "flat"]
