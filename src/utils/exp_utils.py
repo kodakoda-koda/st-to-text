a716e5e4562fa89182090c125c636124197251dd
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-09-24 10:53:49 +0900

    refactor: Remove unused coordinates loss calculation

--- src/utils/exp_utils.py
+++ src/utils/exp_utils.py
@@ -42,35 +42,9 @@ class CustomLoss:
     def __init__(self, loss_weight: Tensor, writer: SummaryWriter):
         super(CustomLoss, self).__init__()
         self.lm_loss_func = nn.CrossEntropyLoss(weight=loss_weight, ignore_index=-100)
-        self.x_idx = [4347, 4482, 6355, 8525, 11116, 11071, 32100, 11864]
-        self.y_idx = [209, 204, 220, 314, 305, 431, 489, 505]
-        self.softmax = nn.Softmax(dim=-1)
         self.writer = writer
 
     def __call__(self, logits: Tensor, labels: Tensor, coords_labels, step: int) -> Tensor:
-        x_prob = self.softmax(logits[:, 2])
-        y_prob = self.softmax(logits[:, 3])
-
-        x_coords = torch.argmax(x_prob, dim=-1).to(torch.device("cpu")).apply_(self.__get_coord__).to(logits.device)
-        y_coords = torch.argmax(y_prob, dim=-1).to(torch.device("cpu")).apply_(self.__get_coord__).to(logits.device)
-        x_dist = torch.abs(x_coords - coords_labels[:, 0])
-        y_dist = torch.abs(y_coords - coords_labels[:, 1])
-
-        x_loss = x_prob.max(dim=-1)[0] * x_dist
-        y_loss = y_prob.max(dim=-1)[0] * y_dist
-
         lm_loss = self.lm_loss_func(logits.view(-1, logits.size(-1)), labels.view(-1))
-        coords_loss = x_loss.mean() + y_loss.mean()
-
         self.writer.add_scalar("Loss/lm_loss", lm_loss.item(), step)
-        self.writer.add_scalar("Loss/coord_loss", coords_loss.item(), step)
-
-        return lm_loss + coords_loss * 0.1
-
-    def __get_coord__(self, i: int) -> int:
-        if i not in self.x_idx + self.y_idx:
-            return 100
-        elif i in self.x_idx:
-            return self.x_idx.index(i) + 1
-        else:
-            return self.y_idx.index(i) + 1
+        return lm_loss
