5065fc8257d6b3d6d1c8a0d5d18058ad38e39899
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-10-01 16:31:01 +0900

    test: Add tests for model forward pass.

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 
 import numpy as np
@@ -10,56 +11,61 @@ from src.model.model import Model
 
 
 class TestModel:
-    max_length = 128
-    n_layers = 12
-    d_model = 768
-    n_heads = 12
-    d_ff = 3072
-    dropout = 0.1
-    n_locations = 100
-    lm_name = "t5-base"
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dtype = torch.bfloat16
+    def __init__(self):
+        args = {
+            "max_length": 128,
+            "n_layers": 12,
+            "d_model": 768,
+            "n_heads": 12,
+            "d_ff": 3072,
+            "dropout": 0.1,
+            "time_range": 30,
+            "max_fluc_range": 10,
+            "n_data": 500,
+            "n_locations": 100,
+            "map_size": 10,
+            "data_dir": "./data/",
+            "decoder_max_length": 64,
+        }
+        self.args = argparse.Namespace(**args)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.dtype = torch.bfloat16
 
-    st_maps = np.load("./data/st_maps.npy")
-    with open("./data/labels.json") as f:
-        labels = json.load(f)
-    tokenizer = AutoTokenizer.from_pretrained(lm_name)
+        tokenizer = AutoTokenizer.from_pretrained("t5-base")
 
-    train_dataset = CustomDataset(st_maps, labels, tokenizer, max_length)
-    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+        train_dataset = CustomDataset(self.args, tokenizer, True)
+        self.train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
 
-    batch = next(iter(train_loader))
-    st_maps = batch["st_maps"].to(device).to(dtype)
-    inst_input_ids = batch["inst_input_ids"].to(device)
-    decoder_input_ids = batch["decoder_input_ids"][:, :-1].to(device)
-    decoder_attention_mask = batch["decoder_attention_mask"][:, :-1].to(device)
-    labels = batch["decoder_input_ids"][:, 1:].to(device)
-
-    model = (
-        Model(
-            n_layers,
-            d_model,
-            n_heads,
-            d_ff,
-            dropout,
-            n_locations,
-            lm_name,
+        self.model = (
+            Model(
+                self.args.n_layers,
+                self.args.d_model,
+                self.args.n_heads,
+                self.args.d_ff,
+                self.args.dropout,
+                self.args.n_locations,
+            )
+            .to(self.device)
+            .to(self.dtype)
         )
-        .to(device)
-        .to(dtype)
-    )
 
     def test_forward(self):
-        outputs = self.model(
-            st_maps=self.st_maps,
-            inst_input_ids=self.inst_input_ids,
-            decoder_input_ids=self.decoder_input_ids,
-            decoder_attention_mask=self.decoder_attention_mask,
-            labels=self.labels,
-        )
-        assert outputs.loss is not None
-        assert outputs.logits is not None
-        assert outputs.loss.item() >= 0.0
-        assert outputs.logits.shape == (16, self.max_length - 1, self.model.t5.config.vocab_size)
-        assert outputs.loss.requires_grad
+        for batch in self.train_loader:
+            st_maps = batch["st_maps"].to(self.device).to(self.dtype)
+            inst_input_ids = batch["inst_input_ids"].to(self.device)
+            decoder_input_ids = batch["decoder_input_ids"].to(self.device)
+            decoder_attention_mask = batch["decoder_attention_mask"].to(self.device)
+            labels = batch["labels"].to(self.device)
+
+            outputs = self.model(
+                st_maps=st_maps,
+                inst_input_ids=inst_input_ids,
+                decoder_input_ids=decoder_input_ids,
+                decoder_attention_mask=decoder_attention_mask,
+                labels=labels,
+            )
+            assert outputs.loss is not None
+            assert outputs.logits is not None
+            assert outputs.loss.item() >= 0.0
+            assert outputs.logits.shape == (16, self.args.decoder_max_length - 1, self.model.t5.config.vocab_size)
+            assert outputs.loss.requires_grad
