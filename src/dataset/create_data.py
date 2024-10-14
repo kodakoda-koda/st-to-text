4c355f4010914ae7e2f5470b06c9170a50ae85da
Souhei Kodama <gundam634rx78.2@gmail.com>
2024-10-14 20:36:23 +0900

    refactor: Improve fluctuation function and simplify labels.

--- src/dataset/create_data.py
+++ src/dataset/create_data.py
@@ -26,34 +26,25 @@ def create_data(time_range: int, max_fluc_range: int, n_data: int, map_size: int
 
     for i in range(n_data):
         # Randomly generate a spot and change
-        fluc_range = np.random.randint(3, max_fluc_range)
-        fluc_index = np.random.randint(3, time_range // 3 - fluc_range - 3)
-        start_value = np.random.uniform(0.2, 0.8)
-        fluc_list = ["increase", "decrease", "peak", "dip", "flat"]
+        fluc_list = ["increase", "decrease", "peak", "trough", "flat"]
         spot = [np.random.randint(1, map_size - 1), np.random.randint(1, map_size - 1)]
-        spot_fluc_pos = np.random.randint(3)  # 0: beggining, 1: middle, 2: end
-        other_fluc_pos = np.random.randint(3)
-        spot_change = random.choice(fluc_list)
-        other_change = random.choice(fluc_list)
+        spot_fluc = random.choice(fluc_list)
+        other_fluc = random.choice(fluc_list)
 
-        if spot_change == other_change:
-            spot_value = fluctuate(time_range, spot_fluc_pos, fluc_range, fluc_index, start_value, spot_change)
-            other_value = spot_value.copy()
-        else:
-            spot_value = fluctuate(time_range, spot_fluc_pos, fluc_range, fluc_index, start_value, spot_change)
-            other_value = fluctuate(time_range, other_fluc_pos, fluc_range, fluc_index, start_value, other_change)
+        spot_values, spot_ind = fluctuate(spot_fluc)
+        other_values, other_ind = fluctuate(other_fluc)
 
         # Replace the spot with the spot value and the surrounding with the other value
         for j in range(map_size):
             for k in range(map_size):
                 if [j, k] == spot:
-                    st_maps[i, :, j, k] = spot_value
+                    st_maps[i, :, j, k] = spot_values
                 elif j == spot[0] and (k == spot[1] - 1 or k == spot[1] + 1):
-                    st_maps[i, :, j, k] = (spot_value + other_value) / 2
+                    st_maps[i, :, j, k] = (spot_values + other_values) / 2
                 elif k == spot[1] and (j == spot[0] - 1 or j == spot[0] + 1):
-                    st_maps[i, :, j, k] = (spot_value + other_value) / 2
+                    st_maps[i, :, j, k] = (spot_values + other_values) / 2
                 else:
-                    st_maps[i, :, j, k] = other_value
+                    st_maps[i, :, j, k] = other_values
         noise = np.random.randn(time_range, map_size, map_size) * 0.02
         st_maps[i] += noise
 
@@ -61,7 +52,7 @@ def create_data(time_range: int, max_fluc_range: int, n_data: int, map_size: int
         coords.append([[[j, k] for k in range(map_size)] for j in range(map_size)])
 
         # Create label text
-        labels.append(label_text(spot, spot_change, spot_fluc_pos, other_change, other_fluc_pos))
+        labels.append(label_text(spot, spot_fluc, spot_ind, other_fluc, other_ind))
         coords_labels.append(spot)
 
     logger.info("Data created")
--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -3,70 +3,95 @@ from typing import List
 import numpy as np
 
 
-def fluctuate(
-    range_: int, change_pos: int, change_range: int, change_index: int, start_value: float, fluctuation: str
-) -> np.ndarray:
-    max_value = np.random.uniform(start_value + 0.2, 1)
-    min_value = np.random.uniform(0, start_value - 0.2)
+def fluctuate(fluc: str):
+    if fluc == "peak":
+        start_val = np.random.uniform(0.2, 0.8)
+        end_val = np.random.uniform(0.2, 0.8)
+        fluc_val = np.random.uniform(max(start_val, end_val) + 0.2, 1.0)
+        fluc_ind = np.random.randint(3, 21)
+        start_ind = np.random.randint(0, fluc_ind - 2)
+        end_ind = np.random.randint(fluc_ind + 1, 24)
+        ind = {"start_ind": start_ind, "fluc_ind": fluc_ind, "end_ind": end_ind}
+    elif fluc == "trough":
+        start_val = np.random.uniform(0.2, 0.8)
+        end_val = np.random.uniform(0.2, 0.8)
+        fluc_val = np.random.uniform(0, min(start_val, end_val) - 0.2)
+        fluc_ind = np.random.randint(3, 21)
+        start_ind = np.random.randint(0, fluc_ind - 2)
+        end_ind = np.random.randint(fluc_ind + 1, 24)
+        ind = {"start_ind": start_ind, "fluc_ind": fluc_ind, "end_ind": end_ind}
+    elif fluc == "flat":
+        start_val = np.random.uniform(0.2, 0.8)
+        ind = {"start_ind": None, "fluc_ind": None, "end_ind": None}
+    elif fluc == "increase":
+        start_val = np.random.uniform(0.2, 0.8)
+        end_val = np.random.uniform(start_val + 0.2, 1.0)
+        start_ind = np.random.randint(0, 20)
+        end_ind = np.random.randint(start_ind + 2, 24)
+        ind = {"start_ind": start_ind, "fluc_ind": None, "end_ind": end_ind}
+    elif fluc == "decrease":
+        start_val = np.random.uniform(0.2, 0.8)
+        end_val = np.random.uniform(0.0, start_val - 0.2)
+        start_ind = np.random.randint(0, 20)
+        end_ind = np.random.randint(start_ind + 2, 24)
+        ind = {"start_ind": start_ind, "fluc_ind": None, "end_ind": end_ind}
+    else:
+        raise ValueError("Invalid fluctuation type")
 
     values = []
-    for i in range(range_):
-        if i < change_pos * 20 + change_index:
-            values.append(start_value)
-
-        elif i < change_pos * 20 + change_index + change_range // 2 + 1:
-            if fluctuation == "increase":
-                diff = (max_value - start_value) / (change_range + 1)
-            elif fluctuation == "decrease":
-                diff = (min_value - start_value) / (change_range + 1)
-            elif fluctuation == "peak":
-                diff = (max_value - start_value) / (change_range // 2 + 1)
-            elif fluctuation == "dip":
-                diff = (min_value - start_value) / (change_range // 2 + 1)
-            else:
-                diff = 0
-            values.append(values[i - 1] + diff)
-
-        elif i < change_pos * 20 + change_index + change_range:
-            if fluctuation == "increase":
-                diff = (max_value - start_value) / (change_range + 1)
-            elif fluctuation == "decrease":
-                diff = (min_value - start_value) / (change_range + 1)
-            elif fluctuation == "peak":
-                diff = (start_value - max_value) / (change_range // 2 + 1)
-            elif fluctuation == "dip":
-                diff = (start_value - min_value) / (change_range // 2 + 1)
-            else:
-                diff = 0
-            values.append(values[i - 1] + diff)
-
-        else:
-            if fluctuation == "increase":
-                values.append(max_value)
-            elif fluctuation == "decrease":
-                values.append(min_value)
-            else:
-                values.append(start_value)
-
-    return np.array(values)
-
-
-def label_text(spot: List[int], spot_change: str, spot_fluc_pos: int, other_change: str, other_fluc_pos: int) -> str:
-    period = {0: "beginning", 1: "middle", 2: "end"}
-    if spot_change != other_change or spot_fluc_pos != other_fluc_pos:
-        if spot_change == "flat":
-            spot_text = f"location {spot} shows a {spot_change}"
-        else:
-            spot_text = f"location {spot} shows a {spot_change} at {period[spot_fluc_pos]}"
-        if other_change == "flat":
-            other_text = f"other locations show a {other_change}"
-        else:
-            other_text = f"other locations show a {other_change} at {period[other_fluc_pos]}"
-
-        return f"{spot_text}, while {other_text}."
-
+    if fluc == "flat":
+        for i in range(24):
+            values.append(start_val)
     else:
-        if spot_change == "flat":
-            return f"all locations show a {spot_change}."
+        for i in range(24):
+            if i < start_ind:
+                values.append(start_val)
+            elif i > end_ind:
+                values.append(end_val)
+            else:
+                if fluc == "peak" or fluc == "trough":
+                    if i < fluc_ind:
+                        values.append(start_val + (fluc_val - start_val) * (i - start_ind) / (fluc_ind - start_ind))
+                    else:
+                        values.append(fluc_val + (end_val - fluc_val) * (i - fluc_ind) / (end_ind - fluc_ind))
+                elif fluc == "increase" or fluc == "decrease":
+                    values.append(start_val + (end_val - start_val) * (i - start_ind) / (end_ind - start_ind))
+
+    return np.array(values), ind
+
+
+def label_text(spot, spot_fluc, spot_ind, other_fluc, other_ind):
+    if spot_fluc == other_fluc and spot_ind == other_ind:
+        if spot_fluc == "flat":
+            return f"all spots are flat"
+        elif spot_fluc == "peak" or spot_fluc == "trough":
+            return f"all spots reach a {spot_fluc} at {spot_ind['fluc_ind']}:00"
+        elif spot_fluc == "increase" or spot_fluc == "decrease":
+            return f"all spots {spot_fluc} from {spot_ind['start_ind']}:00 to {spot_ind['end_ind']}:00"
         else:
-            return f"all locations show a {spot_change} at {period[spot_fluc_pos]}."
+            raise ValueError("Invalid fluctuation type")
+
+    if spot_fluc == "flat":
+        spot_text = f"Spot {spot} is flat"
+    elif spot_fluc == "peak" or spot_fluc == "trough":
+        spot_text = f"Spot {spot} reaches a {spot_fluc} at {spot_ind['fluc_ind']}:00"
+    elif spot_fluc == "increase" or spot_fluc == "decrease":
+        spot_text = f"Spot {spot} {spot_fluc}s from {spot_ind['start_ind']}:00 to {spot_ind['end_ind']}:00"
+    else:
+        raise ValueError("Invalid fluctuation type")
+
+    if other_fluc == "flat":
+        other_text = f"other spots are flat."
+    elif other_fluc == "peak" or other_fluc == "trough":
+        other_text = f"other spots reach a {other_fluc} at {other_ind['fluc_ind']}:00"
+    elif other_fluc == "increase" or other_fluc == "decrease":
+        other_text = f"other spots {other_fluc} from {other_ind['start_ind']}:00 to {other_ind['end_ind']}:00"
+    else:
+        raise ValueError("Invalid fluctuation type")
+
+    if spot_fluc == other_fluc:
+        conj = "and"
+    else:
+        conj = "while"
+
+    return f"{spot_text}, {conj} {other_text}"
